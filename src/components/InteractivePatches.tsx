5ffc987a2915e60f259606e5c562d0db8cebeb02
Nga Vu <vuthiquynhngahvn@gmail.com>
2025-07-23 22:57:12 -0700

    feat: update text labels and styles in InteractivePatches component

--- src/components/InteractivePatches.tsx
+++ src/components/InteractivePatches.tsx
@@ -11,7 +11,7 @@ export default function InteractivePatches() {
             className="patch" 
             points="401.5,557 338.5,671 523,665.5 518.5,734.5 700.5,697 635.5,515.5 401.5,557" 
           />
-          <text x="520" y="640" className="patch-text">About me!</text>
+          <text x="520" y="600" className="patch-text">Nga Vu</text>
         </g>
       </Link>
 
@@ -25,7 +25,7 @@ export default function InteractivePatches() {
         </g>
       </Link>
 
-      <Link href="/resume" id="resume-link" className="patch-link">
+      <Link href="https://drive.google.com/file/d/1gMn9RTJeB4Yf9pdz9AKnXc66aIHU3MYs/view?usp=sharing" id="resume-link" className="patch-link" target="_blank" rel="noopener noreferrer">
         <g className="patch-group">
           <polygon 
             className="patch" 
@@ -45,6 +45,100 @@ export default function InteractivePatches() {
         </g>
       </Link>
 
+      <Link href="" id="job-link" className="patch-link">
+        <g className="patch-group">
+          <polygon
+            className="patch"
+            points="340,671 295,829 516,856 548,750 516,734.5 516,671 340,671"
+          />
+          <text x="420" y="750" className="patch-text" fontSize="20">
+            <tspan x="400" dy="0">Software</tspan>
+            <tspan x="430" dy="35">Engineer</tspan>
+          </text>
+        </g>
+      </Link>
+
+      <Link href="" id="location-link" className="patch-link">
+        <g className="patch-group">
+          <polygon
+            className="patch"
+            points="707.5,704.5 551,733 508,853 726.5,911.5 739.5,894.5 714.5,853 714.5,818.5 733.5,798.5 707.5,704.5"
+          />
+          <text x="610" y="800" className="patch-text">📍 Tempe,AZ</text>
+        </g>
+      </Link>
+
+      <Link href="" id="placeholder1-link" className="patch-link">
+        <g className="patch-group">
+          <polygon
+            className="patch"
+            points="441.5,393.5 403.5,554 633.5,515.5 579.5,380 441.5,393.5"
+          />
+          <text x="500" y="450" className="patch-text">1</text>
+        </g>
+      </Link>
+
+      <Link href="" id="placeholder2-link" className="patch-link">
+        <g className="patch-group">
+          <polygon
+            className="patch"
+            points="287,962.5 299,1077.5 514,1077.5 514,962.5 504.5,856.5 396.5,843.5 299,843.5 287,962.5"
+          />
+          <text x="400" y="920" className="patch-text">2</text>
+        </g>
+      </Link>
+
+      <Link href="" id="placeholder3-link" className="patch-link">
+        <g className="patch-group">
+          <polygon
+            className="patch"
+            points="728,912.5 519,865 519,1073 701.5,1073 728,912.5"
+          />
+          <text x="600" y="950" className="patch-text">3</text>
+        </g>
+      </Link>
+
+      <Link href="" id="placeholder4-link" className="patch-link">
+        <g className="patch-group">
+          <polygon
+            className="patch"
+            points="959.5,671.5 833.5,689.5 804,818.5 795.5,854.5 738.5,892.5 720.5,982.5 883.5,906.5 938,783 959.5,671.5"
+          />
+          <text x="850" y="800" className="patch-text">4</text>
+        </g>
+      </Link>
+
+      <Link href="" id="placeholder5-link" className="patch-link">
+        <g className="patch-group">
+          <polygon
+            className="patch"
+            points="942,779.5 885.5,904 924.5,893 986,1072.5 1109,1072.5 1068,764 942,779.5"
+          />
+          <text x="1020" y="800" className="patch-text">5</text>
+        </g>
+      </Link>
+
+      <Link href="" id="placeholder6-link" className="patch-link">
+        <g className="patch-group">
+          <polygon
+            className="patch"
+            points="1231.5,918 1231.5,1072.5 1112,1072.5 1096,927.5 1231.5,918"
+          />
+          <text x="1150" y="950" className="patch-text">6</text>
+        </g>
+      </Link>
+
+      <Link href="" id="placeholder7-link" className="patch-link">
+        <g className="patch-group">
+          <polygon
+            className="patch"
+            points="1194,376.5 1113.5,520 1161,644 1246,545 1348,564 1194,376.5"
+          />
+          <text x="1194" y="450" className="patch-text">7</text>
+        </g>
+      </Link>
     </svg>
+
+    
   )
 }
